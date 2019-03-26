4d5b6c03b49e9cb646ee06ae7f4ba1f33c38ea8d
Kenec <nnamani.kenechukwu@gmail.com>
2019-03-26 08:55:48 +0100

    setup service 2 for deployment to ECS

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  const today = new Date();
+
+  const hour = today.getUTCHours();
+  const minutes = today.getUTCMinutes();
+  const seconds = today.getUTCSeconds();
+
+  res.status(200).send({ time: `${hour}:${minutes}:${seconds}` }) 
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('App started!')
+});
